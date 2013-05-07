3983b1f9e6d2a84a781d201a5735c0e2d651308c
Tomás Augusto Müller <tomas.am@gmail.com>
2013-05-07 00:23:07 -0300

    Configuration sample.

--- conf/deploy.rb
+++ conf/deploy.rb
@@ -3,11 +3,11 @@ set :repository,  "git@github.com:tomasmuller/playframework-neo4j-template.git"
 
 set :scm, :git
 
-role :web, "codenroll.org"                   # Your HTTP server, Apache/etc
-role :app, "codenroll.org"                   # This may be the same as your `Web` server
-role :db,  "codenroll.org", :primary => true # This is where Rails migrations will run
+role :web, "example.com"                   # Your HTTP server, Apache/etc
+role :app, "example.com"                   # This may be the same as your `Web` server
+role :db,  "example.com", :primary => true # This is where Rails migrations will run
 
-set :port, 22777
+set :port, 22
 
 # The server's user for deploys
 set :user, "deployer"
@@ -23,7 +23,7 @@ set :keep_releases, 5
 
 # the directory where capistrano will setup it's env and clone your VCS and deploy current version etc...
 # read this for more info about Capistrano deploy https://github.com/mpasternacki/capistrano-documentation-support-files/raw/master/default-execution-path/Capistrano%20Execution%20Path.jpg
-set :deploy_to, "/home/deployer/apps/play/#{application}"
+set :deploy_to, "/home/#{user}/apps/play/#{application}"
 
 # You can let it like that
 set :shared_path, "#{deploy_to}/shared"
